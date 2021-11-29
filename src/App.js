1969f63bb7214d7b971881c690c4b8d3c66373c3
Anurag Dhiman <anuragdhiman.nith@gmail.com>
2021-11-30 02:35:39 +0530

    1. Broken the homepage into components 2. Style our menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import './homepage.style.scss';
-import HomePage from "./homepage.component" ;
+import './pages/homepage/homepage.style.scss';
+import HomePage from "./pages/homepage/homepage.component" ;
 
 function App() {
   return (
